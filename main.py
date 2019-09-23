5fbcdaebfab5a16f1b0f51c2c68b8bf427bfa45b
Bohan Hu <hubohancser@outlook.com>
2019-09-23 10:05:08 +0800

    添加类别选择函数，添加二值化

--- main.py
+++ main.py
@@ -26,7 +26,19 @@ def load_label(fn):
     return labels
 
 
-def K_NN(image, train_set, train_label, K=10):
+def threshold(images):
+    return (images >= 127) * 255  # 大于127，则置为1
+
+
+def selectClass(images, labels, classes):
+    index = []
+    a = ()
+    for i in classes:
+        index += np.where(labels == i)[0].tolist()
+    return images[np.array(index)], labels[np.array(index)]
+
+
+def K_NN(image, train_set, train_label, K=10):  # K=1时，就是最近邻算法
     # 将这个768*1的向量复制N份，N是训练集样本数，成为一个N*768的矩阵，并将两个矩阵相减，得到差值
     diff = np.tile(image, (train_set.shape[0], 1)) - train_set
     dist = ((diff ** 2).sum(axis=1)) ** 0.5  # 计算该测试样本和每个训练样本的欧氏距离
@@ -50,9 +62,23 @@ if __name__ == '__main__':
 
     # 统计分类正确率
     sum = 0
+    # print("--------最近邻--------")
+    # for i in range(0, images_test.shape[0]):
+    #     predict = K_NN(images_test[i], images_train, labels_train)
+    #     if predict == labels_test[i]:
+    #         sum = sum + 1
+    #     if i % 100 == 0 and i != 0:
+    #         print(str(i) + ' / 10000 -- Accuracy: ' + str(sum / i))
+    print("--------K近邻（K=10）--------")
+
+    images_train = threshold(images_train)
+    images_test = threshold(images_test)
+    images_train, labels_train = selectClass(threshold(images_train), labels_train, [3,5])
+    images_test, labels_test = selectClass(threshold(images_test), labels_test, [3,5])
+    sum = 0
     for i in range(0, images_test.shape[0]):
         predict = K_NN(images_test[i], images_train, labels_train)
         if predict == labels_test[i]:
             sum = sum + 1
-        if i % 100 == 0 and i != 0:
-            print(str(i) + ' / 10000 -- Accuracy: ' + str(sum / i))
+        if (i + 1) % 100 == 0:
+            print(str(sum) + ' / ' + str(i + 1) + ' -- Accuracy: ' + str(sum / (i + 1)))
